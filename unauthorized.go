1a694ae97f8ae2f75b56c63aae1c0ca30ab94a60
Alexander Shcherbyna <xantax@ukr.net>
2018-05-25 14:39:26 +0200

    401 error

---
+++ unauthorized.go
@@ -0,0 +1,14 @@
+package errors
+
+import "net/http"
+
+var _ Error = Unauthorized{}
+
+// Unauthorized is a 401 HTTP error.
+type Unauthorized struct{ error }
+
+// Code returns HTTP staus code.
+func (e Unauthorized) Code() int { return http.StatusUnauthorized }
+
+// NewUnauthorized is a constructor func for 401 HTTP error.
+func NewUnauthorized(cause error) Unauthorized { return Unauthorized{cause} }
