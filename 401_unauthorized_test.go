06b7438a3463e692355ad3ca06a3812d5dc910d7
Alexander Shcherbyna <xantax@ukr.net>
2018-05-29 09:08:02 +0200

    error 401 tests

--- 401_unauthorized_test.go
+++ 401_unauthorized_test.go
@@ -10,15 +10,15 @@ func TestNewUnauthorized(t *testing.T) {
 	t.Run("Check Unauthorized status code", func(t *testing.T) {
 		var err error
 		err = NewUnauthorized(errors.New("Unauthorized"))
-		unauthorized, ok := err.(Unauthorized)
+		typed, ok := err.(Unauthorized)
 		if !ok {
 			t.Fatalf("error has invalid type: %T", err)
 		}
-		if unauthorized.Code() != http.StatusUnauthorized {
-			t.Errorf("error has invalid status code: %d", unauthorized.Code())
+		if typed.Code() != http.StatusUnauthorized {
+			t.Errorf("error has invalid status code: %d", typed.Code())
 		}
-		if unauthorized.Error() != "Unauthorized" {
-			t.Errorf("error has invalid message: %q", unauthorized.Error())
+		if typed.Error() != "Unauthorized" {
+			t.Errorf("error has invalid message: %q", typed.Error())
 		}
 	})
 }
