478c86428400bee99fea611b173252807bf29711
Alexander Shcherbyna <xantax@ukr.net>
2018-05-29 09:07:33 +0200

    error 400 tests

--- 400_bad_request_test.go
+++ 400_bad_request_test.go
@@ -10,15 +10,15 @@ func TestNewBadRequest(t *testing.T) {
 	t.Run("Check Bad Request status code", func(t *testing.T) {
 		var err error
 		err = NewBadRequest(errors.New("BadRequest"))
-		badRequest, ok := err.(BadRequest)
+		typed, ok := err.(BadRequest)
 		if !ok {
-			t.Error("error has invalid type")
+			t.Fatalf("error has invalid type: %T", err)
 		}
-		if badRequest.Code() != http.StatusBadRequest {
-			t.Error("error has invalid status code")
+		if typed.Code() != http.StatusBadRequest {
+			t.Errorf("error has invalid status code: %d", typed.Code())
 		}
-		if badRequest.Error() != "BadRequest" {
-			t.Error("error has invalid message")
+		if typed.Error() != "BadRequest" {
+			t.Errorf("error has invalid message: %q", typed.Error())
 		}
 	})
 }
