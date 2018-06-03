de08d5c91bd2e37cb4a1af5ccc532bdcdfede490
Alexander Shcherbyna <xantax@ukr.net>
2018-06-03 08:32:25 +0200

    error 405

---
+++ 405_method_not_allowed.go
@@ -0,0 +1,14 @@
+package errors
+
+import "net/http"
+
+var _ Error = MethodNotAllowed{}
+
+// MethodNotAllowed is a 404 HTTP error.
+type MethodNotAllowed struct{ error }
+
+// Code returns HTTP status code.
+func (e MethodNotAllowed) Code() int { return http.StatusMethodNotAllowed }
+
+// NewMethodNotAllowed is a constructor func for 404 HTTP error.
+func NewMethodNotAllowed(cause error) MethodNotAllowed { return MethodNotAllowed{cause} }
