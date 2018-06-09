ea9e8b9009b3c25d57021b428eb6588ffb6cdd42
Alexander Shcherbyna <xantax@ukr.net>
2018-06-09 14:57:51 +0200

    fix typo

--- 405_method_not_allowed.go
+++ 405_method_not_allowed.go
@@ -10,5 +10,5 @@ type MethodNotAllowed struct{ error }
 // Code returns HTTP status code.
 func (e MethodNotAllowed) Code() int { return http.StatusMethodNotAllowed }
 
-// NewMethodNotAllowed is a constructor func for 404 HTTP error.
+// NewMethodNotAllowed is a constructor func for 405 HTTP error.
 func NewMethodNotAllowed(cause error) MethodNotAllowed { return MethodNotAllowed{cause} }
