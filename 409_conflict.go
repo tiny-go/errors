4e95b362f6eef7d71db6d38d0bdf0422350b233c
Alexander Shcherbyna <xantax@ukr.net>
2018-06-10 06:44:50 +0200

    409 error

---
+++ 409_conflict.go
@@ -0,0 +1,14 @@
+package errors
+
+import "net/http"
+
+var _ Error = Conflict{}
+
+// Conflict is a 404 HTTP error.
+type Conflict struct{ error }
+
+// Code returns HTTP status code.
+func (e Conflict) Code() int { return http.StatusConflict }
+
+// NewConflict is a constructor func for 409 HTTP error.
+func NewConflict(cause error) Conflict { return Conflict{cause} }
